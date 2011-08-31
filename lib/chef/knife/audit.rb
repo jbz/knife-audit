339681f118e0f1065ff66132812f5b120348b1ff
J.B. Zimmerman <jzimmerman@mdsol.com>
2011-08-31 14:55:41 -0400

    Knife audit now works and handles --show-nodelist option as well as specific cookbooks specified on the command line

--- lib/chef/knife/audit.rb
+++ lib/chef/knife/audit.rb
@@ -17,58 +17,59 @@
 
 require 'chef/knife'
  
-module KnifeSurvey
-  class Survey < Chef::Knife
+module KnifeAudit
+  class Audit < Chef::Knife
  
     deps do
       require 'chef/cookbook_loader'
       require 'chef/environment'
-#      require 'chef/knife/core/object_loader'
       require 'chef/node'
       require 'chef/run_list'
-#      require 'chef/knife/search'
-#      require 'chef/search/query'
       require 'chef/json_compat' 
       require 'chef/shef/ext'
-   end
+    end
  
-    banner "knife survey <COOKBOOK COOKBOOK ...>"
+    banner "knife audit <COOKBOOK COOKBOOK ...>"
  
-    option :all_versions,
-      :short => "-a",
-      :long => "--all-versions",
-      :description => "Show all cookbook versions."
+    option :show_nodelist,
+      :short => "-s",
+      :long => "--show-nodelist",
+      :description => "Show all nodes running each cookbook"
 
-    
     def run
 
-#      all_args = parse_name_args!
-#      cookbooks = all_args
-      
+      unless @name_args.empty? 
+        display_cookbooks = @name_args 
+      end
+
       self.config = Chef::Config.merge!(config)
    
       # 1) Get a list (hash, actually, with key of 'name') of cookbooks available on the current server/org
-
+      #    unless we've been given a cookbook/cookbooks on the command line
       env		= config[:environment]
       num_versions 	= config[:all_versions] ? "num_versions=all" : "num_versions=1"
-      api_endpoint	= env ? "/environments/#{env}/cookbooks?#{num_versions}" : "/cookbooks?#{num_versions}"
-      cookbook_list	= rest.get_rest(api_endpoint)
-#      format_cookbook_list_for_display(cookbook_list).each do |line|
-#        puts line
-#        ui.msg(line)
-#      end
+      
+      if display_cookbooks.empty?
+        api_endpoint	= env ? "/environments/#{env}/cookbooks?#{num_versions}" : "/cookbooks?#{num_versions}"
+        cookbook_list	= rest.get_rest(api_endpoint)
+      else
+        cookbook_list	= {}
+        display_cookbooks.each do |cookbook_name|
+          api_endpoint	= env ? "/environments/#{env}/cookbooks/#{cookbook_name}" : "cookbooks/#{cookbook_name}"
+          cookbook_list.merge!(rest.get_rest(api_endpoint))
+        end 
+      end
+
+#      puts cookbook_list.inspect
 
       # add count => 0 to each cookbook hash
       cookbook_list.each do |name,book|
         book["count"] = 0 
-      #  puts "Found cookbook with name #{name} : #{book.inspect}"
+        book["nodes"] = []
       end
 
 
-      # 2) Get a list of nodes known to the current server/org
-
-#      node_list		= (env ? Chef::Node.list_by_environment(env) : Chef::Node.list )
-#      output(format_list_for_display(node_list))
+      # 2) Get an array of Chef::Nodes known to the current server/org
 
       query = "*:*"  # find all nodes
 
@@ -85,20 +86,44 @@ module KnifeSurvey
         recipes = node.expand!.recipes.to_a
         node_cookbook_list = recipes.map{ |x| x.match(/[^\:]+/)[0] }.uniq
 
-        puts node_cookbook_list.inspect
-        
-        # 3b) Add the cookbooks/recipes in node's runlist to the node array's 'runlist' hash
+        # 3b) For each cookbook in the node runlist, if it's in our cookbook array increment its count and
+        #     add the node to its running node array
 
-        # 3c) For each recognized cookbook in the runlist, add to that cookbook's count variable
+        node_cookbook_list.each do |cookbook|
+	  if cookbook_list.has_key?(cookbook)
+            # Up the cookbook count
+            cookbook_list[cookbook]["count"] += 1
+            # Add the node to the cookbook's nodes array 
+            cookbook_list[cookbook]["nodes"] << node.name
+          end
+        end
 
-#      end # step 3 iterate end
+      end # step 3 iterate end
 
       # 4) Output total counts for each cookbook in cookbook list
 
+      format_cookbook_audit_list_for_display(cookbook_list).each do |line|
+        ui.msg(line)
+      end
+
       # 5) Output complete node/cookbook array
 
-
     end # 'run' def end
+
+
+    def format_cookbook_audit_list_for_display(item)
+      key_length = item.empty? ? 0 : item.keys.map {|name| name.size }.max + 2
+      if config[:show_nodelist]
+        item.sort.map do |name, cookbook|
+          "#{name.ljust(key_length)} #{cookbook["count"]}   [ #{cookbook["nodes"].join('  ')} ]"
+        end
+      else
+        item.sort.map do |name, cookbook|
+          "#{name.ljust(key_length)} #{cookbook["count"]}"
+        end
+      end
+         
+    end # format_cokbook_audit... def end
 
 
   end #class end
