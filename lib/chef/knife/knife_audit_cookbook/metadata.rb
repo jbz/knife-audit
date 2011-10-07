4431cd448fc8244627a33ecf0f3aa97f6f2c9b5c
J.B. Zimmerman <jzimmerman@mdsol.com>
2011-10-07 12:31:05 -0400

    Added knife_audit cookbook into gem

---
+++ lib/chef/knife/knife_audit_cookbook/metadata.rb
@@ -0,0 +1,6 @@
+maintainer        "J.B. Zimmerman"
+maintainer_email  "jbzimmerman91@gmail.com"
+license           "Apache 2.0"
+description       "A cookbook for saving node auditing data for the knife-audit plugin"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version           "0.0.2"
